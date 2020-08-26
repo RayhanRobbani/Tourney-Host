cda5f7f5c865732f9182b9941cf54c51e1a71bd8
RayhanRobbani <54496007+RayhanRobbani@users.noreply.github.com>
2020-08-26 18:35:59 +0600

    adding php to connect databse

---
+++ dbc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "TourneyHost";
+$dbPassword = "th789456123";
+$dbName = "tourney host";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
